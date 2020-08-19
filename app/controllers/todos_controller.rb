9708d9f924d9f4aaad2b6207ae84d27fa3daa9ee
Danilo Velozo <velozo.dan@gmail.com>
2020-08-18 23:20:53 -0300

    Add Project

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,36 @@
+# frozen_string_literal: true
+
+# nodoc
 class TodosController < ApplicationController
-  def index
+  def index; end
+
+  def all_todos
+    completed = Todo.where(completed: true)
+    pending = Todo.where(completed: false).order(:id)
+    render json: { completed: completed, pending: pending }
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update_attributes!(todo_params)
+      render json: { message: 'Todo item updated successfully' }
+    else
+      render json: { message: 'An error occured' }
+    end
+  end
+
+  def create
+    new_todo = Todo.create!(todo_params)
+    if new_todo
+      render json: { message: 'Todo item created successfully' }
+    else
+      render json: { message: 'An error occured'}
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:id, :title, :completed)
   end
 end
---
+++ db/migrate/20200818021356_create_todos.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# nodoc
+class CreateTodos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
